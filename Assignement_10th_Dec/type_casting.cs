2e3b567cba0c9faf69078be2b80fe023943e1c02
Ashish Vishnu Fulwade <57040506+Fulwade@users.noreply.github.com>
2021-12-11 14:24:37 +0530

    Create type_casting.cs

---
+++ Assignement_10th_Dec/type_casting.cs
@@ -0,0 +1,21 @@
+using System;  
+using System.Collections.Generic;  
+using System. Linq;  
+using System. Text;  
+using System.Threading.Tasks;  
+  
+namespace ConsoleApp2  
+{  
+    class ProgramExplicit  
+    {  
+        static void Main(string[] args)  
+        {  
+   double db = 7896.45;  
+            int xy;  
+  
+            // here we do the cast double to int.  
+            xy = (int)db;  
+            Console.WriteLine(xy);  
+            Console.ReadKey();  
+        }  
+    }
